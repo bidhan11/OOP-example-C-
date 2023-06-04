9a41d3e08915b2210736e3ca5b723aedb0e80af9
Bidhan Marasine <bidhanmarasine88@gmail.com>
2023-06-04 07:02:35 +0545

    OOP concept

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ConsoleApp3;
+using System;
 namespace InheritanceLab
 {
     class Program
@@ -6,6 +7,37 @@ namespace InheritanceLab
         static void Main(string[] args)
         {
             Messages.Greet();
+            LabObject l1 = new LabObject(3,"ROBIN");
+            l1.name = "hema";
+            l1.ShowDetails();
+            LabObject l2 = new LabObject(1, "Arun");
+            l2.ShowDetails();
+            l2.SetID(10);
+        }
+    }
+    class LabObject
+    {
+        private int id;
+        public string name;
+        public LabObject()
+        {
+            this.id = 1;
+            this.name = "Test";
+        }
+        //constructor
+        public LabObject(int id,string name)
+        {
+            this.id = id;
+            this.name = name;   
+
+        }
+        public void ShowDetails()
+        {
+            Console.WriteLine("Name of student " + name + " ID " + id);
+        }
+        public void SetID(int id)
+        {
+            this.id = id;
         }
     }
 }
